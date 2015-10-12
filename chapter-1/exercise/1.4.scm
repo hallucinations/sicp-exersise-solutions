695d855ba9bf3a2747f93ee021d8d50121de7efc
Vasanth Balakrishnan <vasantheb@gmail.com>
2015-10-12 14:54:47 +0530

    Cube root using Newton's method

---
+++ chapter-1/exercise/1.4.scm
@@ -0,0 +1,21 @@
+#lang scheme
+
+(define (cube-root x)
+  (cube-root-iter 1.0 x))
+
+(define (cube-root-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-root-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(define (square x) (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.0000000000001))
+
+(define (cube x) (* x x x))
+
+; (cube-root 64)
