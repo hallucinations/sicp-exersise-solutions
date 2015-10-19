3b16bcbcd214b08d37525b92d18f095f27863b76
Vasanth Balakrishnan <vasantheb@gmail.com>
2015-10-19 18:24:02 +0530

    Factorial using Linear Iteration

---
+++ chapter-1/others/linear-iterative-factorial.scm
@@ -0,0 +1,12 @@
+#lang scheme
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* product counter) (+ counter 1) max-count)))
+
+
+(factorial 6)
