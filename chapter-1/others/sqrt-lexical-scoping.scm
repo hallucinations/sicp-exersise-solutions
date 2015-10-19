0440454ac00348ac6885fbbb3a515fd5fcdb2444
Vasanth Balakrishnan <vasantheb@gmail.com>
2015-10-19 18:23:24 +0530

    Square root using Lexical Scoping

---
+++ chapter-1/others/sqrt-lexical-scoping.scm
@@ -0,0 +1,18 @@
+#lang scheme
+;; Square root function with lexical scoping.
+
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  ;;(define (square x) (x * x))
+  (define (improve guess)
+    (average guess (/ guess x)))
+  (define (average x y) (/ (+ x y) 2))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+
+(sqrt 4)
